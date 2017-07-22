067d1b82eb1d7ac799ecd318dba49a794aaf4070
Pat Hawks <pat@pathawks.com>
2017-07-22 11:45:32 -0500

    More script cleanup

--- publish-cvc4.sh
+++ publish-cvc4.sh
@@ -21,13 +21,13 @@ ANTLR="$1/antlr-3.4/bin/antlr3"
 # build and copy version with assertions
 pushd "$DIR"
 # make clean
-JAVA_CPPFLAGS=$JAVA_CPPFLAGS ./configure production --enable-language-bindings=java --with-antlr-dir=/space/hawks/CVC4/antlr-3.4
+env ANTLR="$ANTLR" JAVA_CPPFLAGS="$JAVA_CPPFLAGS" ./configure production --enable-language-bindings="java" --with-antlr-dir="$1/antlr-3.4"
 #Now just type make, followed by make check or make install, as you like.
 # make
-cp $JAR           $IVY"/libs/cvc4/cvc4-$VERSION.jar"
-cp $LIBCVC4       $IVY"/libs/cvc4/libcvc4-$VERSION.so"
-cp $LIBCVC4JNI    $IVY"/libs/cvc4/libcvc4jni-$VERSION.jar"
-cp $LIBCVC4PARSER $IVY"/libs/cvc4/libcvc4parser-$VERSION.jar"
+cp $JAR           "$IVY/libs/cvc4/cvc4-$VERSION.jar"
+cp $LIBCVC4       "$IVY/libs/cvc4/libcvc4-$VERSION.so"
+cp $LIBCVC4JNI    "$IVY/libs/cvc4/libcvc4jni-$VERSION.jar"
+cp $LIBCVC4PARSER "$IVY/libs/cvc4/libcvc4parser-$VERSION.jar"
 popd
 
 ./publish.sh cvc4 -publishpattern "[artifact]-[revision](-[classifier]).[ext]" -revision "$VERSION"
