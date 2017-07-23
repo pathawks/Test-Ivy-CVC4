df6d0865c16e8d0ae5cff7bbf0bb2bc34b466875
Pat Hawks <pat@pathawks.com>
2017-07-23 18:24:58 -0500

    Don't look for files until after they are created

--- publish-cvc4.sh
+++ publish-cvc4.sh
@@ -9,25 +9,26 @@ fi
 
 IVY="$(pwd)"
 DIR="$1"
-JAR=("$DIR"/builds/*/production/src/bindings/CVC4.jar)
-LIBCVC4=("$DIR"/builds/*/production/lib/libcvc4.so)
-LIBCVC4JNI=("$DIR"/builds/*/production/src/bindings/java/.libs/libcvc4jni.so)
-LIBCVC4PARSER=("$DIR"/builds/*/production/lib/libcvc4parser.so)
 
 JAVA_CPPFLAGS='-I/usr/lib/jvm/java-openjdk/include -I/usr/lib/jvm/java-openjdk/include/linux'
 ANTLR="$1/antlr-3.4/bin/antlr3"
 
 # build and copy version with assertions
 pushd "$DIR"
-VERSION="$(git describe --tags)"
-# make clean
-env ANTLR="$ANTLR" JAVA_CPPFLAGS="$JAVA_CPPFLAGS" ./configure production --enable-language-bindings="java" --with-antlr-dir="$1/antlr-3.4"
-#Now just type make, followed by make check or make install, as you like.
-# make
-cp "${JAR[0]}"           "$IVY/libs/cvc4/cvc4-$VERSION.jar"
-cp "${LIBCVC4[0]}"       "$IVY/libs/cvc4/libcvc4-$VERSION.so"
-cp "${LIBCVC4JNI[0]}"    "$IVY/libs/cvc4/libcvc4jni-$VERSION.jar"
-cp "${LIBCVC4PARSER[0]}" "$IVY/libs/cvc4/libcvc4parser-$VERSION.jar"
+    VERSION="$(git describe --tags)"
+    # make clean
+    env ANTLR="$ANTLR" JAVA_CPPFLAGS="$JAVA_CPPFLAGS" ./configure production --enable-language-bindings="java" --with-antlr-dir="$1/antlr-3.4"
+    ANTLR="$ANTLR" make
+
+    JAR=("$DIR"/builds/*/production/src/bindings/CVC4.jar)
+    LIBCVC4=("$DIR"/builds/*/production/lib/libcvc4.so)
+    LIBCVC4JNI=("$DIR"/builds/*/production/src/bindings/java/.libs/libcvc4jni.so)
+    LIBCVC4PARSER=("$DIR"/builds/*/production/lib/libcvc4parser.so)
+
+    cp "${JAR[0]}"           "$IVY/libs/cvc4/cvc4-$VERSION.jar"
+    cp "${LIBCVC4[0]}"       "$IVY/libs/cvc4/libcvc4-$VERSION.so"
+    cp "${LIBCVC4JNI[0]}"    "$IVY/libs/cvc4/libcvc4jni-$VERSION.jar"
+    cp "${LIBCVC4PARSER[0]}" "$IVY/libs/cvc4/libcvc4parser-$VERSION.jar"
 popd
 
 ./publish.sh cvc4 -publishpattern "[artifact]-[revision](-[classifier]).[ext]" -revision "$VERSION"
