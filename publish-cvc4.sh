6a78579dc74350220ccff704966f6d4e72015aaf
Pat Hawks <pat@pathawks.com>
2017-07-23 12:44:16 -0500

    Look for artifacts in the proper directory

--- publish-cvc4.sh
+++ publish-cvc4.sh
@@ -9,17 +9,17 @@ fi
 
 IVY="$(pwd)"
 DIR="$1"
-JAR=(builds/*/production/src/bindings/CVC4.jar)
-LIBCVC4=(builds/*/production/lib/libcvc4.so)
-LIBCVC4JNI=(builds/*/production/src/bindings/java/.libs/libcvc4jni.so)
-LIBCVC4PARSER=(builds/*/production/lib/libcvc4parser.so)
-VERSION="$(date +"%Y-%m-%d-r")$(svn info | grep "Revision" | awk '{print $2}')"
+JAR=("$DIR"/builds/*/production/src/bindings/CVC4.jar)
+LIBCVC4=("$DIR"/builds/*/production/lib/libcvc4.so)
+LIBCVC4JNI=("$DIR"/builds/*/production/src/bindings/java/.libs/libcvc4jni.so)
+LIBCVC4PARSER=("$DIR"/builds/*/production/lib/libcvc4parser.so)
 
 JAVA_CPPFLAGS='-I/usr/lib/jvm/java-openjdk/include -I/usr/lib/jvm/java-openjdk/include/linux'
 ANTLR="$1/antlr-3.4/bin/antlr3"
 
 # build and copy version with assertions
 pushd "$DIR"
+VERSION="$(git describe --tags)"
 # make clean
 env ANTLR="$ANTLR" JAVA_CPPFLAGS="$JAVA_CPPFLAGS" ./configure production --enable-language-bindings="java" --with-antlr-dir="$1/antlr-3.4"
 #Now just type make, followed by make check or make install, as you like.
