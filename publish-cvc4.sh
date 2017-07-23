9b0df3baa654639fca3dfd802a85a833fb0a8619
Pat Hawks <pat@pathawks.com>
2017-07-23 12:26:16 -0500

    Fix issues in script with Shellcheck
    https://www.shellcheck.net

--- publish-cvc4.sh
+++ publish-cvc4.sh
@@ -9,10 +9,10 @@ fi
 
 IVY="$(pwd)"
 DIR="$1"
-JAR="builds/*/production/src/bindings/CVC4.jar"
-LIBCVC4="builds/*/production/lib/libcvc4.so"
-LIBCVC4JNI="builds/*/production/src/bindings/java/.libs/libcvc4jni.so"
-LIBCVC4PARSER="builds/*/production/lib/libcvc4parser.so"
+JAR=(builds/*/production/src/bindings/CVC4.jar)
+LIBCVC4=(builds/*/production/lib/libcvc4.so)
+LIBCVC4JNI=(builds/*/production/src/bindings/java/.libs/libcvc4jni.so)
+LIBCVC4PARSER=(builds/*/production/lib/libcvc4parser.so)
 VERSION="$(date +"%Y-%m-%d-r")$(svn info | grep "Revision" | awk '{print $2}')"
 
 JAVA_CPPFLAGS='-I/usr/lib/jvm/java-openjdk/include -I/usr/lib/jvm/java-openjdk/include/linux'
@@ -24,10 +24,10 @@ pushd "$DIR"
 env ANTLR="$ANTLR" JAVA_CPPFLAGS="$JAVA_CPPFLAGS" ./configure production --enable-language-bindings="java" --with-antlr-dir="$1/antlr-3.4"
 #Now just type make, followed by make check or make install, as you like.
 # make
-cp $JAR           "$IVY/libs/cvc4/cvc4-$VERSION.jar"
-cp $LIBCVC4       "$IVY/libs/cvc4/libcvc4-$VERSION.so"
-cp $LIBCVC4JNI    "$IVY/libs/cvc4/libcvc4jni-$VERSION.jar"
-cp $LIBCVC4PARSER "$IVY/libs/cvc4/libcvc4parser-$VERSION.jar"
+cp "${JAR[0]}"           "$IVY/libs/cvc4/cvc4-$VERSION.jar"
+cp "${LIBCVC4[0]}"       "$IVY/libs/cvc4/libcvc4-$VERSION.so"
+cp "${LIBCVC4JNI[0]}"    "$IVY/libs/cvc4/libcvc4jni-$VERSION.jar"
+cp "${LIBCVC4PARSER[0]}" "$IVY/libs/cvc4/libcvc4parser-$VERSION.jar"
 popd
 
 ./publish.sh cvc4 -publishpattern "[artifact]-[revision](-[classifier]).[ext]" -revision "$VERSION"
